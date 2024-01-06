dc164cef31177648a5752324aa6fe52f77a785db
oluwakunmi <ubeydatoluwakunmi@gmail.com>
2024-01-06 09:58:09 -0800

    control flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if number > 5:
     print(f"Last digit of {number} is {number % 10} and is greater than 5")
 elif number == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
-elif number < 6:
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+else:
+    print("less than 6 and not 0")
